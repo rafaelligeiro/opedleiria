2624421085d81105ed3faacc44af282e640c345c
Rúben Teixeira <rubenact05@hotmail.com>
2023-11-17 20:56:43 +0000

    Create index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends ('layout.master')
+
+@section('title', '..')
+
+@section('styles')
+  <!--
+        opcional
+        <link rel="stylesheet" href="style_message.css">
+
+    -->
+@endsection
+
+@section('content')
+    Conteúdo da página
+
+@endsection
+
+@section('scripts')
+    <!-- opcional-->
+@endsection
