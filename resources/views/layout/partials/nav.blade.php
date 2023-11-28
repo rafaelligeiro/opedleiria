4be839cf4a56ce4ce4bd34ce0ee6401b54bf1ed7
lilvlad <vladsamodelkin1994@gmail.com>
2023-11-28 15:20:43 +0000

    new nav

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -21,7 +21,7 @@
                     <a class="nav-link" aria-disabled="true">Loja</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" aria-disabled="true">Contactoos</a>
+                    <a class="nav-link" aria-disabled="true">Contactos</a>
                 </li>
             </ul>
             <form id="pesquisa" class="d-flex" role="search">
