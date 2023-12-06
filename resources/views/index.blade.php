44b94d1cd335be356072bf8dc482d8721e182015
Rúben Teixeira <rubenact05@hotmail.com>
2023-12-06 13:49:09 +0000

    font update index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,9 @@
 
 @section('styles')
   <link rel="stylesheet" href="{{asset('css/index.css')}}">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 @endsection
 
 @section('content')
