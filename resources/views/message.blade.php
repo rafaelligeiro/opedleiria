f3cd1a10034992cf5fec187f3e95160b7e8b08c5
rafaelligeiro <81650852+rafaelligeiro@users.noreply.github.com>
2023-12-13 13:23:44 +0000

    m

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -6,6 +6,9 @@
 @vite('resources/css/app.css')
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="{{asset('css/estilos_message.css')}}">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 @endsection
 
 @section('content')
