eff58f85e0007e6c27cb6277f2a73c108fa2f302
Assis2001 <assis2001.psn@gmail.com>
2023-12-14 22:49:29 +0000

    Update eventos.blade.php

--- resources/views/eventos.blade.php
+++ resources/views/eventos.blade.php
@@ -17,24 +17,10 @@
         <h1 class="titulo">Eventos</h1>
     </section>
 
-    <section class="cards">
-        @foreach($eventos as $evento)
-            <div class="card">
-                <img src="{{ asset('storage/imagens_eventos/'.$evento->imagem) }}" alt="{{ $evento->titulo }}">
-                <div class="card-content">
-                    <h3>{{ $evento->titulo }}</h3>
-                    <p>{{ $evento->descricao }}</p>
-                    <a href="{{route('inscricoes.create',$evento)}}" class="my-2 w-full bg-primary-600 hover:bg-primary-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-xl text-md px-5 py-2.5 text-center flex items-center justify-center text-white gap-2">Inscrever-me <i class="fa-solid fa-arrow-right fa-xs" style="color: #ffffff;"></i></i></a>
-                </div>
-            </div>
-        @endforeach
-    </section>
-
-
     <div class="eventos">
-        @foreach($eventos->take(3) as $evento)
+        @foreach($eventos as $evento)
         <div class="evento">
-            <img src="{{$evento->imagem}}" alt="">
+            <img src="{{ asset('storage/imagens_eventos/'.$evento->imagem) }}" alt="{{ $evento->titulo }}">
             <div class="content">
                 <div>
                     <h2 class="tituloEvento">{{ $evento->titulo }}</h2>
@@ -46,7 +32,6 @@
                             <p class="cima">{{ \Carbon\Carbon::parse($evento->data)->format('H:i') }}</p>
                             <p class="meio">{{ \Carbon\Carbon::parse($evento->data)->format('d') }}</p>
                             <p class="baixo">{{ \Carbon\Carbon::parse($evento->data)->format('F') }}</p>
-
                         </div>
                         <div class="box">
                             <p class="cima">Máximo</p>
