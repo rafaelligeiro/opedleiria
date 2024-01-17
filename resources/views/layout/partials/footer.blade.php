502f01f38835cf5593a02178284bfdfa7abd59d1
Rúben Teixeira <rubenact05@hotmail.com>
2024-01-17 23:24:22 +0000

    bue cenas

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -1,23 +1,89 @@
-<footer class="bg-white mt-36">
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="#" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="{{asset('img/logo.svg')}}" class="h-8" alt="Flowbite Logo" />
-            </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Sobre</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Politica de Provacidade</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contactos</a>
-                </li>
-            </ul>
-        </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" class="hover:underline">OPEDLeiria</a>. Todos os Direitos Reservados.</span>
-    </div>
-</footer>
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-body-tertiary text-muted">
+    <!-- Section: Links  -->
+    <section class="" style="padding: 25px 0 50px 0">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              <i class="fas fa-gem me-3"></i>Associação OPEDLeiria
+            </h6>
+            <p>
+              A associação OPEDLeiria é uma organização sem fins lucrativos
+              que tem como objetivo cuidar de pessoas com dificuldades.
+            </p>
+          </div>
+          <!-- Grid column -->
 
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Páginas
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Sobre</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Eventos</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Loja</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Contactos</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Loja
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Loja</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Blog</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Orders</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Help</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">Contacto</h6>
+            <p><i class="fas fa-home me-3"></i>Avenida Heróis de Angola</p>
+            <p>
+              <i class="fas fa-envelope me-3"></i>
+              suporte@opedleiria.pt
+            </p>
+            <p><i class="fas fa-phone me-3"></i> +351 915 386 024 </p>
+            <p><i class="fas fa-print me-3"></i> +351 244 684 422 </p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2024 Direitos Reservados:
+      <a class="text-reset fw-bold" href="http://opedleiria.test/">opedleiria.pt</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
--- resources/views/noticias.blade.php
+++ resources/views/noticias.blade.php
@@ -3,7 +3,6 @@
 @section('title', 'Notícias OPEDLeiria')
 
 @section('styles')
-  @vite('resources/css/app.css')
   <link rel="stylesheet" href="{{asset('css/noticias.css')}}">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
