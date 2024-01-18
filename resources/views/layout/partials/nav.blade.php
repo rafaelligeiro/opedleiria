45eba4a4c2ff05fdb9447a1ceb24fe743afe92e5
Assis2001 <assis2001.psn@gmail.com>
2024-01-18 00:12:07 +0000

    Update nav.blade.php

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -18,6 +18,9 @@
                     <a class="nav-link" href="{{route('eventos')}}">Eventos</a>
                 </li>
                 <li class="nav-item">
+                    <a class="nav-link" href="{{route('noticias')}}">Noticias</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link">Loja</a>
                 </li>
                 <li class="nav-item">
