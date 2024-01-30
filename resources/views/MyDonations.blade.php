4a2b954eaa64c6631574738af9c11e94e970de73
rafaelligeiro <rafa.ligeiro12@gmail.com>
2024-01-30 23:07:14 +0000

    fg

--- resources/views/MyDonations.blade.php
+++ resources/views/MyDonations.blade.php
@@ -14,58 +14,57 @@
 @endsection
 
 @section('content')
-            <div class="card-body">
-                @if(isset($doacao))
-                    <div><strong>Titulo:</strong> {{ $doacao->titulo }} </div>
-                    <div><strong>Quantidade:</strong> {{ $doacao->quantidade }} </div>
-                    <div><strong>Tipo de Pagamento:</strong> {{ $doacao->tipo_pagamento }} </div>
-                    <div><strong>Nome:</strong> {{ $doacao->nome }} </div>
-                    <div><strong>Data:</strong> {{ $doacao->data }} </div>
-                    <div><strong>Descrição:</strong> {{ $doacao->descricao }} </div>
-                    <div><strong>E-mail:</strong> {{ $doacao->email }} </div>
-                @endif
-            </div>
-        </div>
-        <section>
-            <h1 class="titulo">Doações</h1>
-            <div class="tabelaTop">
-                <table cellpadding="0" cellspacing="0" border="0">
-                    <thead>
+<div class="card-body">
+    @if(isset($doacoes) && count($doacoes) > 0)
+        <div class="tabelaTop">
+            <table cellpadding="0" cellspacing="0" border="0">
+                <thead>
+                    <tr>
+                        <th>Quantidade</th>
+                        <th>Tipo de Pagamento</th>
+                        <th>Nome</th>
+                        <th>Data</th>
+                        <th>Descrição</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($doacoes as $doacao)
                         <tr>
-                            <th>Quantidade</th>
-                            <th>Tipo de Pagamento</th>
-                            <th>Nome</th>
-                            <th>Data</th>
-                            <th>Descrição</th>
-                            <th>E-mail</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                </table>
-            </div>
-            <div class="tabela-content">
-                <table cellpadding="0" cellspacing="0" border="0">
-                    <tbody>
-                        @foreach ($doacoes as $doacao)
-                            <tr>
-                                <form id="formulario" action="/MyDonationEdit" method="POST">
-                                 <td>{{ $doacao->quantidade}}€</td>
-                                <td>{{ $doacao->tipo_pagamento}}</td>
-                                <td><input disabled id="nome" class="nome" type="text" value="{{ $doacao->nome}}" data-original-value="{{ $doacao->nome }}"></td>
-                                <td>{{ $doacao->data}}</td>
-                                <td>{{ $doacao->descricao}}</td>
-                                <td>{{ $doacao->email}}</td>
-                                <td class = "actiontd">
-                                    <a href={{"MyDonationsEdit/".$doacao['id']}}><div onclick="OpenName()" id="actionbtnOpen" class="actionbtn"><i class="fas fa-pencil-alt"></i></div></a>
+                            <form id="formulario" action="/MyDonationEdit" method="POST">
+                                <td>{{ $doacao->quantidade }}€</td>
+                                <td>{{ $doacao->tipo_pagamento }}</td>
+                                <td><input disabled id="nome" class="nome" type="text" value="{{ $doacao->nome }}" data-original-value="{{ $doacao->nome }}"></td>
+                                <td>{{ $doacao->data }}</td>
+                                <td>{{ $doacao->descricao }}</td>
+                                <td>{{ $doacao->email }}</td>
+                                <td class="actiontd">
+                                    <a href="{{ "MyDonationsEdit/".$doacao->id }}"><div onclick="OpenName()" id="actionbtnOpen" class="actionbtn"><i class="fas fa-pencil-alt"></i></div></a>
                                 </td>
-                                </form>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </section>
-    </div>
+                            </form>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    @else
+    <div class="tabelaTop_no_Donations">
+            <table cellpadding="0" cellspacing="0" border="0">
+                <tbody>
+                    <tr>
+                        
+                            <div class="no_Donations">
+                                <h1 class="no_Donations_message">Ainda não realizou nenhuma doação.</h1>
+                                <i style="color:rgb(2, 98, 100);" class="far fa-sad-tear fa-3x"></i>
+                            </div> 
+                        
+                    </tr>
+                </tbody>
+            </table>
+        </div>    
+    @endif
+</div>
 @endsection
 
 @section('scripts')
